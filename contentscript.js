58edd33cea06ca5d4bcf2d65fab4042d97b3b51b
Cosmin Stamate <cos.stamate@gmail.com>
2015-02-23 11:33:43 +0200

    Clean up Javascript files (JSLint rules)

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,29 @@
-var cssFile = chrome.extension.getURL("css/vertical.css");
+var cssFile = chrome.extension.getURL('css/vertical.css');
+var timer;
 
-function readyCheck() {
-    var btnNotifications = document.getElementsByClassName('header-notifications')[0];
-    var btnView = document.getElementById('vertical-button');
-    if (!btnView && btnNotifications)
-    {
-        insertButton();
-        clearInterval(timer);
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function removeElement(node) {
+    node.parentNode.removeChild(node);
+}
+
+function applyVertical() {
+    var css = document.createElement('link');
+    css.id = 'verticalcss';
+    css.type = 'text/css';
+    css.rel = 'stylesheet';
+    css.href = cssFile;
+    document.getElementsByTagName('head')[0].appendChild(css);
+}
+
+function toggleView() {
+    var css = document.getElementById('verticalcss');
+    if (css === null) {
+        applyVertical();
+    } else {
+        removeElement(css);
     }
 }
 
@@ -22,32 +39,19 @@ function insertButton() {
     insertAfter(btnView, btnNotifications);
 }
 
-function toggleView() {
-    var css = document.getElementById('verticalcss');
-    if (css == null)
-        applyVertical();
-    else
-        removeElement(css);
+function readyCheck() {
+    var btnNotifications = document.getElementsByClassName('header-notifications')[0];
+    var btnView = document.getElementById('vertical-button');
+
+    if (!btnView && btnNotifications) {
+        insertButton();
+        clearInterval(timer);
+    }
 }
 
-function applyVertical() {
-    var css = document.createElement('link');
-    css.id = 'verticalcss';
-    css.type = 'text/css';
-    css.rel = 'stylesheet';
-    css.href = cssFile;
-    document.getElementsByTagName('head')[0].appendChild(css);
-}
 
-function removeElement(node) {
-    node.parentNode.removeChild(node);
-}
-
-function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-if (document.URL.indexOf("/b/") != -1)
-    var timer = setInterval(readyCheck,100);
+if (document.URL.indexOf('/b/') !== -1) {
+    timer = setInterval(readyCheck, 100);
     // @todo Need better solution than interval checking after pushState,
     //       but readyState always 'complete' after initial DOM load
+}
