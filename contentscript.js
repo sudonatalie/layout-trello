4178065ad286fa257c28558f16c1e879ea8062b3
Ehsan Al-Kooheji <ehsan@al-kooheji.com>
2015-08-25 16:28:05 +0300

    Saved last layout
    The last layout chosen by the user is saved locally and retrieved when the page is reloaded.

--- contentscript.js
+++ contentscript.js
@@ -21,12 +21,15 @@ function toggleView() {
     if (!board.classList.contains(classMixed)) {
         if (!board.classList.contains(classVertical)) {
             board.classList.add(classMixed);
+            chrome.storage.sync.set({'classList': classMixed});
         } else {
             board.classList.remove(classVertical);
+            chrome.storage.sync.remove('classList');
         }
     } else {
         board.classList.remove(classMixed);
         board.classList.add(classVertical);
+        chrome.storage.sync.set({'classList': classVertical});
     }
 }
 
@@ -51,6 +54,11 @@ function readyCheck() {
     if (!btnView && btnNotifications) {
         insertButton();
         insertCss();
+        chrome.storage.sync.get('classList', function (result) {
+            if(result.classList){
+                board.classList.add(result.classList);
+            }
+        });
         clearInterval(timer);
     }
 }
