77b8aaa2f8cbc6a890471911c61dfac8a46652d3
Natalie Perna <nmperna@gmail.com>
2016-06-29 19:11:11 -0400

    Reorder toggle code for clarity.

--- toggle-layout.js
+++ toggle-layout.js
@@ -1,15 +1,15 @@
-var board = document.getElementById('board');
+var board = document.getElementById('board'),
+    classVertical = 'layout-trello-vertical',
+    classMixed = 'layout-trello-mixed';
 
-if (!board.classList.contains(classMixed)) {
-    if (!board.classList.contains(classVertical)) {
-        board.classList.add(classMixed);
-        chrome.storage.sync.set({'classList': classMixed});
-    } else {
-        board.classList.remove(classVertical);
-        chrome.storage.sync.remove('classList');
-    }
+if (board.classList.contains(classMixed)) {
+  board.classList.remove(classMixed);
+  board.classList.add(classVertical);
+  chrome.storage.sync.set({'classList': classVertical});
+} else if (board.classList.contains(classVertical)) {
+  board.classList.remove(classVertical);
+  chrome.storage.sync.remove('classList');
 } else {
-    board.classList.remove(classMixed);
-    board.classList.add(classVertical);
-    chrome.storage.sync.set({'classList': classVertical});
+  board.classList.add(classMixed);
+  chrome.storage.sync.set({'classList': classMixed});
 }
