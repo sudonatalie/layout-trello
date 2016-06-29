91e973637ef77c1a29945c9acca0bc86f03213e9
Natalie Perna <nmperna@gmail.com>
2016-06-29 19:22:22 -0400

    Auto-format.

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    // Match Trello board pages only
-    var regex = new RegExp(/.*:\/\/trello\.com\/b\/.*/);
-    var match = regex.exec(tab.url);
+  // Match Trello board pages only
+  var regex = new RegExp(/.*:\/\/trello\.com\/b\/.*/);
+  var match = regex.exec(tab.url);
 
-    // Setup page for layout changes and show page action
-    if(match && changeInfo.status == 'complete') {
-      chrome.tabs.executeScript(null, { file: 'setup-css.js' });
-      chrome.pageAction.show(tabId);
-    }
+  // Setup page for layout changes and show page action
+  if (match && changeInfo.status == 'complete') {
+    chrome.tabs.executeScript(null, {file: 'setup-css.js'});
+    chrome.pageAction.show(tabId);
+  }
 });
 
-chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, { file: 'toggle-layout.js' });
+chrome.pageAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript(null, {file: 'toggle-layout.js'});
 });
--- setup-css.js
+++ setup-css.js
@@ -1,23 +1,23 @@
 var cssFile = chrome.extension.getURL('css/layout.css'),
-    board = document.getElementById('board'),
-    classVertical = 'layout-trello-vertical',
-    classMixed = 'layout-trello-mixed';
+  board = document.getElementById('board'),
+  classVertical = 'layout-trello-vertical',
+  classMixed = 'layout-trello-mixed';
 
 function insertCss() {
-    if (document.getElementById('layoutcss') === null) {
-        var css = document.createElement('link');
-        css.id = 'layoutcss';
-        css.type = 'text/css';
-        css.rel = 'stylesheet';
-        css.href = cssFile;
-        document.getElementsByTagName('head')[0].appendChild(css);
-    }
+  if (document.getElementById('layoutcss') === null) {
+    var css = document.createElement('link');
+    css.id = 'layoutcss';
+    css.type = 'text/css';
+    css.rel = 'stylesheet';
+    css.href = cssFile;
+    document.getElementsByTagName('head')[0].appendChild(css);
+  }
 }
 
 insertCss();
 
 chrome.storage.sync.get('classList', function (result) {
-    if(result.classList){
-        board.classList.add(result.classList);
-    }
+  if (result.classList) {
+    board.classList.add(result.classList);
+  }
 });
