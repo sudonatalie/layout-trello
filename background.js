16e5038146c49a92f43c465750e2e532e5d2099d
Natalie Perna <nmperna@gmail.com>
2016-06-30 12:55:09 -0400

    Hide page action when navigate away from board.

--- background.js
+++ background.js
@@ -4,9 +4,12 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   var match = regex.exec(tab.url);
 
   // Setup page for layout changes and show page action
-  if (match && changeInfo.status == 'complete') {
+  if (match) {
     chrome.tabs.executeScript(null, {file: 'setup-css.js'});
     chrome.pageAction.show(tabId);
+  }
+  else {
+    chrome.pageAction.hide(tabId);
   }
 });
 
