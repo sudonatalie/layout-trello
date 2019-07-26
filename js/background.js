6ee41201cf598deb670657560ae8c06e91b03cab
Natalie Chouinard <chouinard.nm@gmail.com>
2019-07-25 21:48:52 -0700

    Use declarativeContent API to show/hide page action

--- js/background.js
+++ js/background.js
@@ -1,21 +1,36 @@
+const boardUrl = 'trello.com/b/';
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   // Match Trello board pages only
-  var regex = new RegExp(/.*:\/\/trello\.com\/b\/.*/);
-  var match = regex.exec(tab.url);
+  const match = tab.url.includes(boardUrl);
 
   // Setup page for layout changes and show page action
   if (match) {
     chrome.tabs.executeScript(null, {
       file: 'js/setup-css.js'
     });
-    chrome.pageAction.show(tabId);
-  } else {
-    chrome.pageAction.hide(tabId);
   }
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {
     file: 'js/toggle-layout.js'
+  });
+});
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            urlContains: boardUrl
+          }
+        })
+      ],
+      actions: [
+        new chrome.declarativeContent.ShowPageAction()
+      ]
+    }]);
   });
 });
