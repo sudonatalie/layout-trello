2c258c3c068b5998d6606a3913fdf1ad312f529f
Marein <git@marein.net>
2019-09-12 18:44:10 +0200

    Listen for changes to stored mode, fixes #53

--- js/init.js
+++ js/init.js
@@ -11,6 +11,17 @@ function syncState() {
   });
 }
 
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  let board = document.getElementById('board');
+  let board_id = location.pathname.split('/')[2];
+  let storageKey = 'classList_' + board_id;
+
+  if (namespace == 'sync' && storageKey in changes) {
+    board.classList.remove(changes[storageKey].oldValue);
+    board.classList.add(changes[storageKey].newValue);
+  }
+});
+
 function toggleLayout() {
   const classVertical = 'layout-trello-vertical';
   const classMixed = 'layout-trello-mixed';
